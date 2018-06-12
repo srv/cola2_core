fe8d737d89fb26659be2ac261677d0664d7ae0f2
Narcis Palomeras <narcispr@gmail.com>
2018-06-12 16:46:40 +0200

    Add message for gazebo simulator.

--- cola2_sim/src/dynamics.py
+++ cola2_sim/src/dynamics.py
@@ -26,6 +26,7 @@ import tf
 from nav_msgs.msg import Odometry
 from cola2_msgs.msg import Setpoints
 from cola2_msgs.msg import BodyForceReq
+from gazebo_msgs.msg import ModelState
 
 # Import srv
 from cola2_sim.srv import SimulatedCurrents
@@ -56,6 +57,7 @@ class Dynamics:
 
         # Create publisher
         self.pub_odom = rospy.Publisher(self.name + "/odometry", Odometry, queue_size=2)
+        self.pub_odom_gazebo = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size=2)
 
         # Create subscribers
         rospy.Subscriber(self.namespace + "controller/thruster_setpoints", Setpoints, self.update_thrusters,
@@ -465,6 +467,16 @@ class Dynamics:
         br = tf.TransformBroadcaster()
         br.sendTransform((self.p[0], self.p[1], self.p[2]), orientation,
                          odom.header.stamp, odom.child_frame_id, odom.header.frame_id)
+
+        ##################################################################
+        #        GAZEBO MESSAGE FOR SIMULATION                       #####
+        ##################################################################
+        gazebo_odom = ModelState()
+        gazebo_odom.model_name = 'girona500'
+        gazebo_odom.pose = odom.pose.pose
+        gazebo_odom.reference_frame = 'world'
+        self.pub_odom_gazebo.publish(gazebo_odom)
+        ##################################################################
 
     def get_config(self):
         """ Get config from config file """
