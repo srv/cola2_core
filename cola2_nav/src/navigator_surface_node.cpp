2a1725a2cc35651731990be007106c78ced7dfe5
alejandroc-programa <alejandro.colom@fueib.org>
2025-09-16 12:26:12 +0200

    navigator_surface_node.cpp modified adapted to asv; sim_auv_nav_sensors_node.py -> sim_asv_nav_sensors_node.py

--- cola2_nav/src/navigator_surface_node.cpp
+++ cola2_nav/src/navigator_surface_node.cpp
@@ -13,10 +13,10 @@ class NavigatorSurfaceNode : public EKFSurface2D
 private:
   // Subscribers
   ros::Subscriber sub_gps_;             // [x y]
-  ros::Subscriber sub_dvl_;             // [vx vy vz]
+  // ros::Subscriber sub_dvl_;             // [vx vy vz]
   ros::Subscriber sub_imu_;             // [roll pitch yaw vroll vpitch vyaw]
   ros::Subscriber sub_sound_velocity_;  // sound velocity from SVS
-  ros::Subscriber sub_altitude_;        // altitude from seafloor
+  // ros::Subscriber sub_altitude_;        // altitude from seafloor
 
 public:
   /**
@@ -34,11 +34,11 @@ NavigatorSurfaceNode::NavigatorSurfaceNode()
   // clang-format off
   // Init subscribers
   sub_gps_ = nh_.subscribe("gps", 2, &EKFBaseROS::updatePositionGPSMsg, reinterpret_cast<EKFBaseROS*>(this));
-  sub_dvl_ = nh_.subscribe("dvl", 2, &EKFBaseROS::updateVelocityDVLMsg, reinterpret_cast<EKFBaseROS*>(this));
+  // sub_dvl_ = nh_.subscribe("dvl", 2, &EKFBaseROS::updateVelocityDVLMsg, reinterpret_cast<EKFBaseROS*>(this));
   sub_imu_ = nh_.subscribe("imu", 2, &EKFBaseROS::updateIMUMsg, reinterpret_cast<EKFBaseROS*>(this));
   // Other data
   sub_sound_velocity_ = nh_.subscribe("sound_velocity", 2, &EKFBaseROS::updateSoundVelocityMsg, reinterpret_cast<EKFBaseROS*>(this));
-  sub_altitude_ = nh_.subscribe("altitude", 2, &EKFBaseROS::updateAltitudeMsg, reinterpret_cast<EKFBaseROS*>(this));
+  // sub_altitude_ = nh_.subscribe("altitude", 2, &EKFBaseROS::updateAltitudeMsg, reinterpret_cast<EKFBaseROS*>(this));
   // clang-format on
 }
 
--- cola2_sim/src/sim_asv_nav_sensors_node.py
+++ cola2_sim/src/sim_asv_nav_sensors_node.py
@@ -49,16 +49,16 @@ class SimAUVNavSensors(object):
         # Initialize other vars
         self.has_odom = False
         self.has_old_odom = False
-        self.simulate_altidude = True  # Simulate until altitude received from simulator
+        # self.simulate_altidude = True  # Simulate until altitude received from simulator
         self.ned = NED(self.latitude, self.longitude, 0.0)  # NED frame
 
         # Set up diagnostics
         self.diagnostic_gps = DiagnosticHelper("gps", rospy.get_name())
         self.diagnostic_gps.set_enabled(True)
-        self.diagnostic_depth = DiagnosticHelper("pressure", rospy.get_name())
-        self.diagnostic_depth.set_enabled(True)
-        self.diagnostic_dvl = DiagnosticHelper("dvl", rospy.get_name())
-        self.diagnostic_dvl.set_enabled(True)
+        # self.diagnostic_depth = DiagnosticHelper("pressure", rospy.get_name())
+        # self.diagnostic_depth.set_enabled(True)
+        # self.diagnostic_dvl = DiagnosticHelper("dvl", rospy.get_name())
+        # self.diagnostic_dvl.set_enabled(True)
         self.diagnostic_imu = DiagnosticHelper("imu", rospy.get_name())
         self.diagnostic_imu.set_enabled(True)
 
@@ -67,14 +67,14 @@ class SimAUVNavSensors(object):
             self.pub_gps = rospy.Publisher(self.ns + 'navigator/gps', NavSatFix, queue_size=2)
         if self.usbl_period > 0:
             self.pub_usbl = rospy.Publisher(self.ns + 'navigator/usbl', PoseWithCovarianceStamped, queue_size=2)
-        if self.depth_period > 0:
-            self.pub_depth = rospy.Publisher(self.ns + 'navigator/pressure', FluidPressure, queue_size=2)
-            self.pub_sound_vel = rospy.Publisher(self.ns + 'navigator/sound_velocity', Float32Stamped, queue_size=2)
-            self.pub_temperature = rospy.Publisher(
-                self.ns + 'valeport_sound_velocity/temperature', Temperature, queue_size=2)
-        if self.dvl_period > 0:
-            self.pub_dvl = rospy.Publisher(self.ns + 'navigator/dvl', DVL, queue_size=2)
-            self.pub_altitude = rospy.Publisher(self.ns + 'navigator/altitude', Range, queue_size=2)
+        # if self.depth_period > 0:
+        #     self.pub_depth = rospy.Publisher(self.ns + 'navigator/pressure', FluidPressure, queue_size=2)
+        #     self.pub_sound_vel = rospy.Publisher(self.ns + 'navigator/sound_velocity', Float32Stamped, queue_size=2)
+        #     self.pub_temperature = rospy.Publisher(
+        #         self.ns + 'valeport_sound_velocity/temperature', Temperature, queue_size=2)
+        # if self.dvl_period > 0:
+        #     self.pub_dvl = rospy.Publisher(self.ns + 'navigator/dvl', DVL, queue_size=2)
+        #     self.pub_altitude = rospy.Publisher(self.ns + 'navigator/altitude', Range, queue_size=2)
         if self.imu_period > 0:
             self.pub_imu = rospy.Publisher(self.ns + 'navigator/imu', Imu, queue_size=2)
 
@@ -82,7 +82,7 @@ class SimAUVNavSensors(object):
         rospy.Subscriber(self.ns + 'dynamics/odometry', Odometry, self.update_odometry, queue_size=1)
 
         # Altitude from the simulator
-        rospy.Subscriber(self.ns + 'dynamics/altitude', Range, self.update_altitude, queue_size=1)
+        # rospy.Subscriber(self.ns + 'dynamics/altitude', Range, self.update_altitude, queue_size=1)
 
         # Get transforms
         found = False
@@ -94,19 +94,19 @@ class SimAUVNavSensors(object):
                     _, gps_xyz, gps_rpy = self.tf_handler.get_transform(self.ns[1:] + 'gps')
                     self.tf_gps = transform_from_tf(gps_xyz, gps_rpy)
                     rospy.loginfo("gps tf loaded")
-                # Depth
-                if self.depth_period > 0:
-                    _, depth_xyz, depth_rpy = self.tf_handler.get_transform(self.ns[1:] + 'pressure')
-                    self.tf_depth = transform_from_tf(depth_xyz, depth_rpy)
-                    rospy.loginfo("depth tf loaded")
-                # DVL
-                if self.dvl_period > 0:
-                    _, dvl_xyz, dvl_rpy = self.tf_handler.get_transform(self.ns[1:] + 'dvl')
-                    # self.tf_dvl = transform_from_tf(dvl_xyz, dvl_rpy)  # not standard inverse
-                    v = np.array([[dvl_xyz[0], dvl_xyz[1], dvl_xyz[2]]]).T
-                    r = tf.transformations.euler_matrix(dvl_rpy[0], dvl_rpy[1], dvl_rpy[2])[:3, :3]
-                    self.tf_dvl = v, r.T  # special case (coordinates from base_link to sensor to transform velocity)
-                    rospy.loginfo("dvl tf loaded")
+                # # Depth
+                # if self.depth_period > 0:
+                #     _, depth_xyz, depth_rpy = self.tf_handler.get_transform(self.ns[1:] + 'pressure')
+                #     self.tf_depth = transform_from_tf(depth_xyz, depth_rpy)
+                #     rospy.loginfo("depth tf loaded")
+                # # DVL
+                # if self.dvl_period > 0:
+                #     _, dvl_xyz, dvl_rpy = self.tf_handler.get_transform(self.ns[1:] + 'dvl')
+                #     # self.tf_dvl = transform_from_tf(dvl_xyz, dvl_rpy)  # not standard inverse
+                #     v = np.array([[dvl_xyz[0], dvl_xyz[1], dvl_xyz[2]]]).T
+                #     r = tf.transformations.euler_matrix(dvl_rpy[0], dvl_rpy[1], dvl_rpy[2])[:3, :3]
+                #     self.tf_dvl = v, r.T  # special case (coordinates from base_link to sensor to transform velocity)
+                #     rospy.loginfo("dvl tf loaded")
                 # IMU
                 if self.imu_period > 0:
                     _, imu_xyz, imu_rpy = self.tf_handler.get_transform(self.ns[1:] + 'imu_filter')
@@ -128,10 +128,10 @@ class SimAUVNavSensors(object):
         # Init simulated sensors
         if self.gps_period > 0:
             rospy.Timer(rospy.Duration(self.gps_period), self.publish_gps)
-        if self.depth_period > 0:
-            rospy.Timer(rospy.Duration(self.depth_period), self.publish_depth)
-        if self.dvl_period > 0:
-            rospy.Timer(rospy.Duration(self.dvl_period), self.publish_dvl)
+        # if self.depth_period > 0:
+        #     rospy.Timer(rospy.Duration(self.depth_period), self.publish_depth)
+        # if self.dvl_period > 0:
+        #     rospy.Timer(rospy.Duration(self.dvl_period), self.publish_dvl)
         if self.imu_period > 0:
             rospy.Timer(rospy.Duration(self.imu_period), self.publish_imu)
         if self.usbl_period > 0:
@@ -149,23 +149,23 @@ class SimAUVNavSensors(object):
             'longitude': (self.ns + "navigator/ned_longitude", 3.0333),
             'water_density': (self.ns + "navigator/water_density", 1030.0),
             # private
-            'sea_bottom_depth': ("sea_bottom_depth", 1),
+            # 'sea_bottom_depth': ("sea_bottom_depth", 1),
             'sound_speed': ("sound_speed", 1500.0),
             'gps_period': ("gps_period", 0.5),
-            'depth_period': ("depth_period", 0.5),
-            'dvl_period': ("dvl_period", 0.5),
+            # 'depth_period': ("depth_period", 0.5),
+            # 'dvl_period': ("dvl_period", 0.5),
             'imu_period': ("imu_period", 0.5),
             'usbl_period': ("usbl_period", 5.0),
             # cov
             'gps_position_covariance': ("gps_position_covariance", [0.25, 0.25]),
-            'depth_pressure_covariance': ("depth_pressure_covariance", 0.01),
-            'dvl_velocity_covariance': ("dvl_velocity_covariance", [0.0015, 0.0015, 0.0015]),
+            # 'depth_pressure_covariance': ("depth_pressure_covariance", 0.01),
+            # 'dvl_velocity_covariance': ("dvl_velocity_covariance", [0.0015, 0.0015, 0.0015]),
             'imu_orientation_covariance': ("imu_orientation_covariance", [0.0001, 0.0001, 0.0001]),
             'usbl_position_covariance': ("usbl_position_covariance", 0.5),
             # output cov
             'output_gps_position_covariance': ("output_gps_position_covariance", [0.5, 0.5]),
-            'output_depth_pressure_covariance': ("output_depth_pressure_covariance", 0.1),
-            'output_dvl_velocity_covariance': ("output_dvl_velocity_covariance", [0.02, 0.02, 0.02]),
+            # 'output_depth_pressure_covariance': ("output_depth_pressure_covariance", 0.1),
+            # 'output_dvl_velocity_covariance': ("output_dvl_velocity_covariance", [0.02, 0.02, 0.02]),
             'output_imu_orientation_covariance': ("output_imu_orientation_covariance", [0.1, 0.1, 0.1]),
             'output_usbl_position_covariance': ("output_usbl_position_covariance", 3.0),
             # usbl start at depth bigger than
@@ -182,13 +182,13 @@ class SimAUVNavSensors(object):
                                                              msg.pose.pose.orientation.w])
         self.has_odom = True
 
-    def update_altitude(self, msg):
-        """If altitude is computed outside, stop computing our own."""
-        self.simulate_altidude = False
-        if (msg.min_range < msg.range < msg.max_range):
-            self.altitude = msg.range
-        else:
-            self.altitude = -1.0              # invalid altitude
+    # def update_altitude(self, msg):
+    #     """If altitude is computed outside, stop computing our own."""
+    #     self.simulate_altidude = False
+    #     if (msg.min_range < msg.range < msg.max_range):
+    #         self.altitude = msg.range
+    #     else:
+    #         self.altitude = -1.0              # invalid altitude
 
     def publish_gps(self, event):
         """Publish GPS according to the current position and if close to surface."""
@@ -219,9 +219,9 @@ class SimAUVNavSensors(object):
         gps.position_covariance[4] = self.output_gps_position_covariance[1]
         gps.position_covariance[8] = 1.0
         gps.position_covariance_type = gps.COVARIANCE_TYPE_DIAGONAL_KNOWN
-        # Good GPS data only near to surface
-        if self.odom.pose.pose.position.z > 1.0:
-            gps.status.status = gps.status.STATUS_NO_FIX
+        # # Good GPS data only near to surface
+        # if self.odom.pose.pose.position.z > 1.0:
+        #     gps.status.status = gps.status.STATUS_NO_FIX
         # Publish
         self.pub_gps.publish(gps)
         # Diagnostic message
@@ -302,52 +302,52 @@ class SimAUVNavSensors(object):
         self.diagnostic_depth.report_valid_data(event.current_real)
         self.diagnostic_depth.publish(event.current_real)
 
-    def publish_dvl(self, event):
-        """Publish DVL according to the current velocity."""
-        # Exit if no odom
-        if not self.has_odom:
-            return
-        # Measurement
-        u = self.odom.twist.twist.linear.x + np.random.normal(0.0, np.sqrt(self.dvl_velocity_covariance[0]))
-        v = self.odom.twist.twist.linear.y + np.random.normal(0.0, np.sqrt(self.dvl_velocity_covariance[1]))
-        w = self.odom.twist.twist.linear.z + np.random.normal(0.0, np.sqrt(self.dvl_velocity_covariance[2]))
-        vel = np.array([u, v, w])
-        # Velocity is computed at the gravity center but we want the velocity at the sensor.
-        # Vdvl = V + (v.ang.z x dist(dvl->))
-        ang_vel = np.array([self.odom.twist.twist.angular.x,
-                            self.odom.twist.twist.angular.y, self.odom.twist.twist.angular.z])
-        vel = vel + np.cross(ang_vel, self.tf_dvl[0].ravel())
-        dvl = self.tf_dvl[1].dot(vel)  # rotate the dvl
-        # If simulated altitude, compute it
-        if self.simulate_altidude:
-            self.altitude = self.sea_bottom_depth - self.odom.pose.pose.position.z
-            if self.altitude > 0.5 and self.altitude < 60:
-                # Altitude
-                msg = Range()
-                msg.header.stamp = event.current_real
-                msg.header.frame_id = this_node.get_namespace_no_initial_dash() + '/dvl_altitude'
-                msg.radiation_type = msg.ULTRASOUND
-                msg.field_of_view = 0.2
-                msg.min_range = 0.5
-                msg.max_range = 80.0
-                msg.range = self.altitude
-                self.pub_altitude.publish(msg)
-        # DVL
-        msg = DVL()
-        msg.header.stamp = event.current_real
-        msg.header.frame_id = this_node.get_namespace_no_initial_dash() + '/dvl'
-        msg.velocity.x = dvl[0]
-        msg.velocity.y = dvl[1]
-        msg.velocity.z = dvl[2]
-        msg.velocity_covariance[0] = self.output_dvl_velocity_covariance[0]
-        msg.velocity_covariance[4] = self.output_dvl_velocity_covariance[1]
-        msg.velocity_covariance[8] = self.output_dvl_velocity_covariance[2]
-        msg.altitude = self.altitude
-        self.pub_dvl.publish(msg)
-        # Diagnostic message
-        self.diagnostic_dvl.set_level_and_message(DiagnosticStatus.OK)
-        self.diagnostic_dvl.report_valid_data(event.current_real)
-        self.diagnostic_dvl.publish(event.current_real)
+    # def publish_dvl(self, event):
+    #     """Publish DVL according to the current velocity."""
+    #     # Exit if no odom
+    #     if not self.has_odom:
+    #         return
+    #     # Measurement
+    #     u = self.odom.twist.twist.linear.x + np.random.normal(0.0, np.sqrt(self.dvl_velocity_covariance[0]))
+    #     v = self.odom.twist.twist.linear.y + np.random.normal(0.0, np.sqrt(self.dvl_velocity_covariance[1]))
+    #     w = self.odom.twist.twist.linear.z + np.random.normal(0.0, np.sqrt(self.dvl_velocity_covariance[2]))
+    #     vel = np.array([u, v, w])
+    #     # Velocity is computed at the gravity center but we want the velocity at the sensor.
+    #     # Vdvl = V + (v.ang.z x dist(dvl->))
+    #     ang_vel = np.array([self.odom.twist.twist.angular.x,
+    #                         self.odom.twist.twist.angular.y, self.odom.twist.twist.angular.z])
+    #     vel = vel + np.cross(ang_vel, self.tf_dvl[0].ravel())
+    #     dvl = self.tf_dvl[1].dot(vel)  # rotate the dvl
+    #     # If simulated altitude, compute it
+    #     if self.simulate_altidude:
+    #         self.altitude = self.sea_bottom_depth - self.odom.pose.pose.position.z
+    #         if self.altitude > 0.5 and self.altitude < 60:
+    #             # Altitude
+    #             msg = Range()
+    #             msg.header.stamp = event.current_real
+    #             msg.header.frame_id = this_node.get_namespace_no_initial_dash() + '/dvl_altitude'
+    #             msg.radiation_type = msg.ULTRASOUND
+    #             msg.field_of_view = 0.2
+    #             msg.min_range = 0.5
+    #             msg.max_range = 80.0
+    #             msg.range = self.altitude
+    #             self.pub_altitude.publish(msg)
+    #     # DVL
+    #     msg = DVL()
+    #     msg.header.stamp = event.current_real
+    #     msg.header.frame_id = this_node.get_namespace_no_initial_dash() + '/dvl'
+    #     msg.velocity.x = dvl[0]
+    #     msg.velocity.y = dvl[1]
+    #     msg.velocity.z = dvl[2]
+    #     msg.velocity_covariance[0] = self.output_dvl_velocity_covariance[0]
+    #     msg.velocity_covariance[4] = self.output_dvl_velocity_covariance[1]
+    #     msg.velocity_covariance[8] = self.output_dvl_velocity_covariance[2]
+    #     msg.altitude = self.altitude
+    #     self.pub_dvl.publish(msg)
+    #     # Diagnostic message
+    #     self.diagnostic_dvl.set_level_and_message(DiagnosticStatus.OK)
+    #     self.diagnostic_dvl.report_valid_data(event.current_real)
+    #     self.diagnostic_dvl.publish(event.current_real)
 
     def publish_imu(self, event):
         """Publish IMU according to the current orientation."""
